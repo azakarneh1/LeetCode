1060f064172595fbdd252a8e5b88b967150aeea4
zak <zakarneh21@hotmail.comn>
2026-02-24 02:31:59 +0300

    1071

---
+++ 1071.php
@@ -0,0 +1,61 @@
+<?php
+
+class Solution
+{
+
+    /**
+     * @param String str1
+     * @param String str2
+     * @return String
+     */
+    /**
+     * @param String $str1
+     * @param String $str2
+     * @return String
+     */
+    function gcdOfStrings($str1, $str2)
+    {
+        if ($str1 . $str2 !== $str2 . $str1) {
+            return '';
+        }
+        $len = $this->gcd(strlen($str1), strlen($str2));
+
+        return substr($str1, 0, $len);
+    }
+
+    private function gcd($l1, $l2)
+    {
+        return $l2 == 0 ? $l1 : $this->gcd($l2, $l1 % $l2);
+    }
+}
+
+$solution = new Solution();
+echo $solution->gcdOfStrings("ABCABC", "ABC");
+
+
+/*
+ * Example 1:
+
+Input: str1 = "ABCABC", str2 = "ABC"
+
+Output: "ABC"
+
+Example 2:
+
+Input: str1 = "ABABAB", str2 = "ABAB"
+
+Output: "AB"
+
+Example 3:
+
+Input: str1 = "LEET", str2 = "CODE"
+
+Output: ""
+
+Example 4:
+
+Input: str1 = "AAAAAB", str2 = "AAA"
+
+Output: ""
+ *
+ */
