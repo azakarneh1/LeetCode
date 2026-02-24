65b3f06ad78290580fbc217f521ac424e07d4ee1
zak <zakarneh21@hotmail.comn>
2026-02-25 00:04:21 +0300

    1431

--- 1071.php
+++ 1071.php
@@ -4,11 +4,6 @@ class Solution
 {
 
     /**
-     * @param String str1
-     * @param String str2
-     * @return String
-     */
-    /**
      * @param String $str1
      * @param String $str2
      * @return String
